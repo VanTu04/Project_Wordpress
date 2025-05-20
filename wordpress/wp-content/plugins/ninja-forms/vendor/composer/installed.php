5adb127c36c8b264e87bd24349716ebac566e9c0
VanTu04 <141947683+VanTu04@users.noreply.github.com>
2025-05-20 21:42:00 +0700

    f

---
+++ wordpress/wp-content/plugins/ninja-forms/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'saturday-drive/ninja-forms',
+        'pretty_version' => 'dev-40cb306af2df88c27a79ace435892df01c282878',
+        'version' => 'dev-40cb306af2df88c27a79ace435892df01c282878',
+        'reference' => '40cb306af2df88c27a79ace435892df01c282878',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'saturday-drive/ninja-forms' => array(
+            'pretty_version' => 'dev-40cb306af2df88c27a79ace435892df01c282878',
+            'version' => 'dev-40cb306af2df88c27a79ace435892df01c282878',
+            'reference' => '40cb306af2df88c27a79ace435892df01c282878',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
